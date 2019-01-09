16c3a6afc88f9c7acb53f2170632acc60960b218
Zach Gibson <zach.gibson@taillight.com>
2019-01-09 12:53:11 -0600

    Updates

---
+++ precache-manifest.cb6d16dacc8328bc2917c6971ff2e3be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fdfed2d8cbb4246729d",
+    "url": "/my-apps/static/css/main.ddfad3df.chunk.css"
+  },
+  {
+    "revision": "6fdfed2d8cbb4246729d",
+    "url": "/my-apps/static/js/main.6fdfed2d.chunk.js"
+  },
+  {
+    "revision": "16e313c197533d12a0da",
+    "url": "/my-apps/static/js/1.16e313c1.chunk.js"
+  },
+  {
+    "revision": "a436bf1ccd7ae0ac5b90",
+    "url": "/my-apps/static/js/runtime~main.a436bf1c.js"
+  },
+  {
+    "revision": "5e1f50063c85585be5135c6175793e6a",
+    "url": "/my-apps/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-apps/precache-manifest.426bebf1b51d9eec0c5dbbf826bb8e2b.js"
+  "/my-apps/precache-manifest.cb6d16dacc8328bc2917c6971ff2e3be.js"
 );
 
 workbox.clientsClaim();
