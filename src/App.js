f185f47845a90cc607334f109a5a6869e900705e
Zach Gibson <zach.gibson@taillight.com>
2019-01-09 12:52:44 -0600

    Add react player and work on responsive shell

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
+
 import './App.css';
 
 class App extends Component {
     render() {
-        return <h1>Gucci</h1>;
+        return (
+            <div
+                style={{
+                    position: 'relative',
+                    width: '50vw',
+                    // maxWidth: 560,
+                    height: '28.125vw',
+                    // maxHeight: 315,
+                    // height: '100vh',
+                    marginLeft: 'auto',
+                    marginRight: 'auto'
+                    // paddingTop: 160
+                }}
+            >
+                <ReactPlayer
+                    className="react-player"
+                    url="https://www.youtube.com/watch?v=pAmAI3wpK_U"
+                    width="100%"
+                    height="100%"
+                />
+            </div>
+        );
     }
 }
 
